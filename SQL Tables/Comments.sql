a3ff0e2960ccb52567cb9165edc7fc57c2d9165e
jacobar44 <38545420+jacobar44@users.noreply.github.com>
2021-03-09 15:15:09 -0500

    Update Comments.sql

--- SQL Tables/Comments.sql
+++ SQL Tables/Comments.sql
@@ -7,5 +7,5 @@ Timestamps Time not null,
 Primary key (UID,EID),
 Foreign key (UID) References Users,
 Foreign key (EID) References Event_At,
-Check (Rating<=0 And Rating<=5)
+Check (Rating>=0 And Rating<=5)
 )
