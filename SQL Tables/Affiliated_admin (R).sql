d03e54c52d471c06bbcea17ae8675f68ca76418e
jacobar44 <38545420+jacobar44@users.noreply.github.com>
2021-03-09 15:56:15 -0500

    Update Affiliated_admin (R).sql

--- SQL Tables/Affiliated_admin (R).sql
+++ SQL Tables/Affiliated_admin (R).sql
@@ -1,7 +1,5 @@
 Create Table Affiliated_Admin(
 UID integer,
-University_Name char(50) not null,
 Primary key (UID),
 Foreign key (UID) References Users,
-Foreign key (University_Name) References University,
 )
